608dbd6996df7fa3353a8b50b0e1a20bd4d57b82
SHAIAPOUF21 <86307121+SHAIAPOUF21@users.noreply.github.com>
2021-06-28 18:01:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 # Calculator Logic
+import tkinter
+
+root = tkinter.Tk()
+root.title("Calculator")
+
+expression = ""
+
+# Create functions
+#Add, clear, and calculate
+def add(value):
+    global expression
+    expression += value
+    label_result.config(text=expression)
+    
+def clear():
+    global expression
+    expression = ""
+    label_result.config(text=expression)
+    
+def calculate():
+    pass
